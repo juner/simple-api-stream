d27f3b17d50953f318f99f6c8bdae7a7363f89d5
juner <juner@juner.net>
2025-07-25 15:59:15 +0900

    remove #state / add #factor

--- src/xml/XMLTextToSimpleSAXParser.ts
+++ src/xml/XMLTextToSimpleSAXParser.ts
@@ -12,17 +12,6 @@ import {
 } from "./event";
 import { SimpleSAXHandler } from "./interface";
 
-const parseState = Object.freeze({
-  Text: "Text",
-  Comment: "Comment",
-  Cdata: "Cdata",
-  Doctype: "Doctype",
-  Tag: "Tag",
-  XMLDeclaration: "XMLDeclaration",
-  DisplayingXML: "DisplayingXML",
-});
-type ParseState = typeof parseState[keyof typeof parseState];
-
 const CDATA_PREFIX = "<![CDATA[";
 const CDATA_SUFFIX = "]]>";
 const BLOCK_PREFIX = "<";
@@ -46,38 +35,49 @@ export class XMLTextToSimpleSAXParser extends Error {
 
 export class SimpleSAXParseXMLBuffer {
   #buffer: string = "";
-  #state: ParseState = parseState.Text;
   #handler: Partial<SimpleSAXHandler>;
   #acc: string = "";
+  #cursor: number = 0;
+  #factor!: (flush: boolean) => { required?: true };
+
   get buffer() {
     return this.#buffer;
   }
+
   get state() {
-    return this.#state;
+    return this.#factor.name.slice(1);
   }
+
   get acc() {
     return this.#acc;
   }
+
   #status() {
     return {
       buffer: this.#buffer,
-      state: this.#state,
+      state: this.state,
       acc: this.#acc,
     };
   }
+
   constructor({ handler }: { handler: Partial<SimpleSAXHandler> }) {
     this.#handler = handler;
+    this.#factor = this.#text;
   }
+
   enqueue(chunk: string) {
     this.#buffer += chunk;
     this.#parseBuffer();
   }
+
   flush() {
     this.#parseBuffer(true);
   }
+
   error(reasone: unknown) {
     this.#handler?.onError?.(reasone);
   }
+
   /**
    * make error new SimpleSAXParseXMLBufferError
    * @param message error message
@@ -93,190 +93,36 @@ export class SimpleSAXParseXMLBuffer {
     (options ??= {}).cause = cause;
     return new XMLTextToSimpleSAXParser(message, options);
   }
+
+  /**
+   * make not complete error
+   * @returns
+   */
   #makeNotCompleteError() {
     return this.#makeError(`not complete syntax error. buffer:${this.#buffer}`);
   }
+
+  /**
+   * make syntax error
+   * @param message
+   * @param source
+   * @returns
+   */
+  #makeSyntaxError(message: string, source: string) {
+    return this.#makeError(`${message}: ${source}`, {
+      cause: {
+        syntax: source,
+      }
+    });
+  }
   #parseBuffer(flush: boolean = false): void {
     try {
-      let cursor = 0;
-      while (cursor < this.#buffer.length) {
-        switch (this.#state) {
-          case parseState.Text: {
-            const nextOpen = this.#buffer.indexOf(BLOCK_PREFIX, cursor);
-            if (nextOpen === -1) {
-              this.#acc += this.#buffer.slice(cursor);
-              cursor = this.#buffer.length;
-              break;
-            }
-
-            this.#acc += this.#buffer.slice(cursor, nextOpen);
-            if (this.#acc.trim() !== "") {
-              this.#handler.onText?.(new TextEvent(this.#acc));
-            }
-
-            cursor = nextOpen;
-            this.#acc = "";
-
-            const remaining = this.#buffer.slice(cursor);
-            const remaingin_upper = remaining.toUpperCase();
-
-            if (remaining.startsWith(COMMENT_PREFIX)) {
-              this.#state = parseState.Comment;
-            } else if (remaining.startsWith(CDATA_PREFIX)) {
-              this.#state = parseState.Cdata;
-            } else if (remaingin_upper.startsWith(DOCTYPE_PREFIX)) {
-              this.#state = parseState.Doctype;
-            } else if (remaining.startsWith(XML_STYLESHEET_DECLARATION_PREFIX)) {
-              this.#state = parseState.DisplayingXML;
-            } else if (remaining.startsWith(XML_DECLARATION_PREFIX)) {
-              this.#state = parseState.XMLDeclaration;
-            } else if (
-              CDATA_PREFIX.startsWith(remaining) ||
-              DOCTYPE_PREFIX.startsWith(remaingin_upper) ||
-              COMMENT_PREFIX.startsWith(remaining) ||
-              XML_STYLESHEET_DECLARATION_PREFIX.startsWith(remaining) ||
-              XML_DECLARATION_PREFIX.startsWith(remaining)
-            ) {
-              this.#buffer = this.#buffer.slice(cursor); // 不完全トークン
-              if (flush) throw this.#makeNotCompleteError();
-              return;
-            } else if (remaining.startsWith(DECLARATION_PREFIX)) {
-              const end = this.#buffer.indexOf(DECLARATION_SUFFIX, cursor);
-              if (end === -1) {
-                this.#buffer = this.#buffer.slice(cursor);
-                if (flush) throw this.#makeNotCompleteError();
-                return;
-              }
-              cursor = end + DECLARATION_SUFFIX.length;
-              continue;
-            } else {
-              this.#state = parseState.Tag;
-            }
-            break;
-          }
-
-          case parseState.Comment: {
-            const end = this.#buffer.indexOf(COMMENT_SUFFIX, cursor);
-            if (end < 0) {
-              this.#buffer = this.#buffer.slice(cursor);
-              if (flush) throw this.#makeNotCompleteError();
-              return;
-            }
-            const content = this.#buffer.slice(cursor + COMMENT_PREFIX.length, end);
-            this.#handler.onComment?.(new CommentEvent(content));
-            cursor = end + COMMENT_SUFFIX.length;
-            this.#state = parseState.Text;
-            break;
-          }
-
-          case parseState.Cdata: {
-            const end = this.#buffer.indexOf(CDATA_SUFFIX, cursor);
-            if (end < 0) {
-              this.#buffer = this.#buffer.slice(cursor);
-              if (flush) throw this.#makeNotCompleteError();
-              return;
-            }
-            const content = this.#buffer.slice(cursor + CDATA_PREFIX.length, end);
-            this.#handler.onCdata?.(new CdataEvent(content));
-            cursor = end + CDATA_SUFFIX.length;
-            this.#state = parseState.Text;
-            break;
-          }
-
-          case parseState.Doctype: {
-            const endBracket = this.#buffer.indexOf(DOCTYPE_BLOCK_SUFFIX, cursor);
-            const blockStart = this.#buffer.indexOf(DOCTYPE_BLOCK_START, cursor);
-            const endBlock = this.#buffer.indexOf(BLOCK_SUFFIX, cursor);
-
-            const mode = endBlock < 0 ? undefined : 0 <= blockStart && blockStart < endBlock ? "block" : "simple";
-
-            if (mode === "simple" && 0 <= endBlock) {
-              const next = endBlock + BLOCK_SUFFIX.length;
-              const content = this.#buffer.slice(cursor, next);
-              this.#handler.onDoctype?.(this.#parseDoctype(content));
-              cursor = next;
-              this.#acc = "";
-              this.#state = parseState.Text;
-              continue;
-            }
-            if (mode === "block" && 0 <= endBracket) {
-              const next = endBracket + DOCTYPE_BLOCK_SUFFIX.length;
-              const content = this.#buffer.slice(cursor, next);
-              this.#handler.onDoctype?.(this.#parseDoctype(content));
-              cursor = next;
-              this.#acc = "";
-              this.#state = parseState.Text;
-              continue;
-            }
-            this.#buffer = this.#buffer.slice(cursor);
-            if (flush) throw this.#makeNotCompleteError();
-            return;
-          }
-
-          case parseState.DisplayingXML: {
-            const end = this.#buffer.indexOf(DECLARATION_SUFFIX, cursor);
-            if (end === -1) {
-              this.#buffer = this.#buffer.slice(cursor);
-              if (flush) throw this.#makeNotCompleteError();
-              return;
-            }
-            const acc = this.#buffer.slice(cursor, end);
-            const attrs = this.#parseAttributes(acc);
-            if (attrs.type && attrs.href) {
-              this.#handler.onDisplayingXML?.(
-                new DisplayingXMLEvent(attrs.type, attrs.href)
-              );
-            } else {
-              throw this.#makeError(`Invalid xml-stylesheet declaration: ${this.#acc}`, {
-                cause: {
-                  syntax: this.#acc,
-                }
-              });
-            }
-            cursor = end + DECLARATION_SUFFIX.length;
-            this.#state = parseState.Text;
-            break;
-          }
-
-          case parseState.XMLDeclaration: {
-            const end = this.#buffer.indexOf(DECLARATION_SUFFIX, cursor);
-            if (end === -1) {
-              this.#buffer = this.#buffer.slice(cursor);
-              if (flush) throw this.#makeNotCompleteError();
-              return;
-            }
-            const acc = this.#buffer.slice(cursor, end);
-            const attrs = this.#parseAttributes(acc);
-            this.#handler.onXmlDeclaration?.(
-              new XMLDeclarationEvent(
-                attrs.version ?? "1.0",
-                attrs.encoding ?? "UTF-8",
-                attrs.standalone === "no" ? "no" : "yes"
-              )
-            );
-            cursor = end + DECLARATION_SUFFIX.length;
-            this.#state = parseState.Text;
-            break;
-          }
-
-          case parseState.Tag: {
-            const end = this.#buffer.indexOf(BLOCK_SUFFIX, cursor);
-            if (end === -1) {
-              this.#buffer = this.#buffer.slice(cursor);
-              if (flush) throw this.#makeNotCompleteError();
-              return;
-            }
-            const acc = this.#buffer.slice(cursor, end + BLOCK_SUFFIX.length);
-            this.#processTag(acc);
-            cursor = end + BLOCK_SUFFIX.length;
-            this.#state = parseState.Text;
-            this.#acc = "";
-            break;
-          }
-
-          default:
-            throw this.#makeError("Unknown parse state");
-        }
+      this.#cursor = 0;
+      while (this.#cursor < this.#buffer.length) {
+        const { required } = this.#factor(flush);
+        if (required)
+          if (flush) throw this.#makeNotCompleteError();
+          else return;
       }
       if (flush && this.#acc.length > 0) {
         const text = this.#acc;
@@ -297,6 +143,166 @@ export class SimpleSAXParseXMLBuffer {
       return;
     }
   }
+  // #region factors
+  #text(): { required?: true } {
+    const nextOpen = this.#buffer.indexOf(BLOCK_PREFIX, this.#cursor);
+    if (nextOpen === -1) {
+      this.#acc += this.#buffer.slice(this.#cursor);
+      this.#cursor = this.#buffer.length;
+      return {};
+    }
+
+    this.#acc += this.#buffer.slice(this.#cursor, nextOpen);
+    if (this.#acc.trim() !== "") {
+      this.#handler.onText?.(new TextEvent(this.#acc));
+    }
+
+    this.#cursor = nextOpen;
+    this.#acc = "";
+
+    const remaining = this.#buffer.slice(this.#cursor);
+    const remaingin_upper = remaining.toUpperCase();
+
+    if (remaining.startsWith(COMMENT_PREFIX)) {
+      this.#factor = this.#comment;
+    } else if (remaining.startsWith(CDATA_PREFIX)) {
+      this.#factor = this.#cdata;
+    } else if (remaingin_upper.startsWith(DOCTYPE_PREFIX)) {
+      this.#factor = this.#doctype;
+    } else if (remaining.startsWith(XML_STYLESHEET_DECLARATION_PREFIX)) {
+      this.#factor = this.#displayingXML;
+    } else if (remaining.startsWith(XML_DECLARATION_PREFIX)) {
+      this.#factor = this.#xmlDeclaration;
+    } else if (
+      CDATA_PREFIX.startsWith(remaining) ||
+      DOCTYPE_PREFIX.startsWith(remaingin_upper) ||
+      COMMENT_PREFIX.startsWith(remaining) ||
+      XML_STYLESHEET_DECLARATION_PREFIX.startsWith(remaining) ||
+      XML_DECLARATION_PREFIX.startsWith(remaining)
+    ) {
+      this.#buffer = this.#buffer.slice(this.#cursor); // 不完全トークン
+      return { required: true };
+    } else if (remaining.startsWith(DECLARATION_PREFIX)) {
+      const end = this.#buffer.indexOf(DECLARATION_SUFFIX, this.#cursor);
+      if (end === -1) {
+        this.#buffer = this.#buffer.slice(this.#cursor);
+        return { required: true };
+      }
+      this.#cursor = end + DECLARATION_SUFFIX.length;
+    } else {
+      this.#factor = this.#tag;
+    }
+    return {};
+  }
+  #comment(): { required?: true } {
+    const end = this.#buffer.indexOf(COMMENT_SUFFIX, this.#cursor);
+    if (end < 0) {
+      this.#buffer = this.#buffer.slice(this.#cursor);
+      return { required: true };
+    }
+    const content = this.#buffer.slice(this.#cursor + COMMENT_PREFIX.length, end);
+    this.#handler.onComment?.(new CommentEvent(content));
+    this.#cursor = end + COMMENT_SUFFIX.length;
+    this.#factor = this.#text;
+    return {};
+  }
+  #cdata(): { required?: true } {
+    const end = this.#buffer.indexOf(CDATA_SUFFIX, this.#cursor);
+    if (end < 0) {
+      this.#buffer = this.#buffer.slice(this.#cursor);
+      return { required: true };
+    }
+    const content = this.#buffer.slice(this.#cursor + CDATA_PREFIX.length, end);
+    this.#handler.onCdata?.(new CdataEvent(content));
+    this.#cursor = end + CDATA_SUFFIX.length;
+    this.#factor = this.#text;
+    return {};
+  }
+  #doctype(): { required?: true } {
+    const endBracket = this.#buffer.indexOf(DOCTYPE_BLOCK_SUFFIX, this.#cursor);
+    const blockStart = this.#buffer.indexOf(DOCTYPE_BLOCK_START, this.#cursor);
+    const endBlock = this.#buffer.indexOf(BLOCK_SUFFIX, this.#cursor);
+
+    const mode = endBlock < 0 ? undefined : 0 <= blockStart && blockStart < endBlock ? "block" : "simple";
+
+    if (mode === "simple" && 0 <= endBlock) {
+      const next = endBlock + BLOCK_SUFFIX.length;
+      const content = this.#buffer.slice(this.#cursor, next);
+      this.#handler.onDoctype?.(this.#parseDoctype(content));
+      this.#cursor = next;
+      this.#acc = "";
+      this.#factor = this.#text;
+      return {};
+    }
+    if (mode === "block" && 0 <= endBracket) {
+      const next = endBracket + DOCTYPE_BLOCK_SUFFIX.length;
+      const content = this.#buffer.slice(this.#cursor, next);
+      this.#handler.onDoctype?.(this.#parseDoctype(content));
+      this.#cursor = next;
+      this.#acc = "";
+      this.#factor = this.#text;
+      return {};
+    }
+    this.#buffer = this.#buffer.slice(this.#cursor);
+    return { required: true };
+  }
+  #displayingXML(): { required?: true } {
+    const end = this.#buffer.indexOf(DECLARATION_SUFFIX, this.#cursor);
+    if (end === -1) {
+      this.#buffer = this.#buffer.slice(this.#cursor);
+      return { required: true };
+    }
+    const acc = this.#buffer.slice(this.#cursor, end);
+    const attrs = this.#parseAttributes(acc);
+    if (attrs.type && attrs.href) {
+      this.#handler.onDisplayingXML?.(
+        new DisplayingXMLEvent(attrs.type, attrs.href)
+      );
+    } else {
+      throw this.#makeError(`Invalid xml-stylesheet declaration: ${this.#acc}`, {
+        cause: {
+          syntax: this.#acc,
+        }
+      });
+    }
+    this.#cursor = end + DECLARATION_SUFFIX.length;
+    this.#factor = this.#text;
+    return {};
+  }
+  #xmlDeclaration(): { required?: true } {
+    const end = this.#buffer.indexOf(DECLARATION_SUFFIX, this.#cursor);
+    if (end === -1) {
+      this.#buffer = this.#buffer.slice(this.#cursor);
+      return { required: true };
+    }
+    const acc = this.#buffer.slice(this.#cursor, end);
+    const attrs = this.#parseAttributes(acc);
+    this.#handler.onXmlDeclaration?.(
+      new XMLDeclarationEvent(
+        attrs.version ?? "1.0",
+        attrs.encoding ?? "UTF-8",
+        attrs.standalone === "no" ? "no" : "yes"
+      )
+    );
+    this.#cursor = end + DECLARATION_SUFFIX.length;
+    this.#factor = this.#text;
+    return {};
+  }
+  #tag(): { required?: true } {
+    const end = this.#buffer.indexOf(BLOCK_SUFFIX, this.#cursor);
+    if (end === -1) {
+      this.#buffer = this.#buffer.slice(this.#cursor);
+      return { required: true };
+    }
+    const acc = this.#buffer.slice(this.#cursor, end + BLOCK_SUFFIX.length);
+    this.#processTag(acc);
+    this.#cursor = end + BLOCK_SUFFIX.length;
+    this.#factor = this.#text;
+    this.#acc = "";
+    return {};
+  }
+  // #endregion
+
   #parseAttributes(source: string): Record<string, string> {
     const attrs: Record<string, string> = {};
     const attrRegex = /\s*([a-zA-Z0-9_:.-]+)=(?:"([^"]*)"|'([^']*)')/g;
@@ -312,7 +318,7 @@ export class SimpleSAXParseXMLBuffer {
     const matches = /^<![Dd][Oo][Cc][Tt][Yy][Pp][Ee]\s+(?<root>[^\s]+)(?:\s+(?<dtdType>[Pp][Uu][Bb][Ll][Ii][Cc]|[Ss][Yy][Ss][Tt][Ee][Mm]))?(?:\s+"(?<uri1>[^"]+)")?(?:\s+"(?<uri2>[^"]+)")?(?:\s+\[(?<declarations>[\S\s]+)\])?>$/m.exec(source);
     if (!matches)
       throw this.#makeSyntaxError("Invalid doctype syntax", source);
-    const {root, dtdType: dtdType_, uri1, uri2, declarations:declarations_} = matches.groups ?? {};
+    const { root, dtdType: dtdType_, uri1, uri2, declarations: declarations_ } = matches.groups ?? {};
     const dtdType = dtdType_?.toUpperCase();
     const declarations = ((declarations) => {
       if ((declarations?.length ?? 0) === 0) return undefined;
@@ -335,13 +341,6 @@ export class SimpleSAXParseXMLBuffer {
       });
     return new DoctypeSimpleEvent(root, {
       declarations,
-    });
-  }
-  #makeSyntaxError(message:string, source: string) {
-    return this.#makeError(`${message}: ${source}`, {
-      cause: {
-        syntax: source,
-      }
     });
   }
 
@@ -393,10 +392,11 @@ export class SimpleSAXParseXMLBuffer {
     }
   }
 }
-function* next(regexp:RegExp, source: string) {
+/** iterate Regexp.exec */
+function* next(regexp: RegExp, source: string) {
   console.assert(regexp.global);
-  let result : RegExpExecArray | null = null;
-  while((result = regexp.exec(source)) !== null) {
+  let result: RegExpExecArray | null = null;
+  while ((result = regexp.exec(source)) !== null) {
     yield result;
   };
 }
